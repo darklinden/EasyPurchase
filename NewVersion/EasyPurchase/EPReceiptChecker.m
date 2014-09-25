21b830d42d3150d7cc395bf1207b81417b359141
darklinden <darklinden@gmail.com>
2014-09-25 14:22:21 +0800

    add version assert

--- NewVersion/EasyPurchase/EPReceiptChecker.m
+++ NewVersion/EasyPurchase/EPReceiptChecker.m
@@ -54,6 +54,9 @@
                      refreshable:(BOOL)refreshable
                       completion:(EPReceiptCheckerCompletionHandle)completionHandle
 {
+    NSAssert([bundleId isEqualToString:[[NSBundle mainBundle] bundleIdentifier]], @"bundle id not equal");
+    NSAssert([version isEqualToString:[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]], @"version not equal");
+    
     EPReceiptChecker *checker = [[EPReceiptChecker alloc] init];
     checker.ticket = [[ObjHolder sharedHolder] pushObject:checker];
     checker.srcBundleId = (NSString *)bundleId;
